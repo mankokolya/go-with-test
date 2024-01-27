95b743c76ea3684bca1d1a2c2ef4947f78f78c0b
mankokolya <mankokolya@gmail.com>
2024-01-27 16:23:52 +0000

    Learn mocking

---
+++ mocking/mocking.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"time"
+)
+
+const finalWord = "Go!"
+const countdownStart = 3
+
+type Sleeper interface {
+	Sleep()
+}
+
+type SpySleeper struct {
+	Calls int
+}
+
+type DefaultSleeper struct{}
+
+func (d *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
+}
+
+func (s *SpySleeper) Sleep() {
+	s.Calls++
+}
+
+func main() {
+	sleeper := &DefaultSleeper{}
+	CountDown(os.Stdout, sleeper)
+	fmt.Println()
+}
+
+func CountDown(out io.Writer, sleeper Sleeper) {
+	for i := countdownStart; i > 0; i-- {
+		fmt.Fprintln(out, i)
+		sleeper.Sleep()
+	}
+
+	fmt.Fprint(out, finalWord)
+}
